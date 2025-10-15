88fb1394b02db276c89b8925a1c22e36985c3127
Shubham yadav <140435929+code0era@users.noreply.github.com>
2025-10-15 22:52:58 +0530

    path

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,11 +26,11 @@ app.use(express.json({ limit: "100mb" })); // increase limit to avoid "payload t
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
-if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname,"../frontend/dist")));
-  
-  app.get("/.*/",(req,res)=>{
-    res.sendFile(path.join(__dirname,"../frontend", "dist","index.html"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/:path(*)", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   })
 }
 
